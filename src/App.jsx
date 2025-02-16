b2cdb93a0a85d0460db74753f300843ef70c4927
Monica-R <moniroka_339@hotmail.com>
2025-02-16 17:48:55 +0000

    Routes and styling in progress...

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
 import './App.css'
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import Sidebar from './components/Sidebar/Sidebar';
 import Home from './pages/Home';
+import AddExpense from './pages/AddExpense';
+import EditExpense from './pages/EditExpense';
+import ExpenseDetails from './pages/ExpenseDetails';
+import Summary from './pages/Summary';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
 
   return (
     <>
-      <Home />
+      <Header />
+      <Sidebar />
+      <Routes>
+        <Route path="/" element={ <Home /> } />
+      </Routes>
+      <Footer />
     </>
   )
 }
