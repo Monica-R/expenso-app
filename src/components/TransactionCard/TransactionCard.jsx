816439c1e7f2b777850b8088bbfc2fe4980a2412
Monica-R <moniroka_339@hotmail.com>
2025-02-18 21:07:57 +0000

    feat: delete button added -not completed-

--- src/components/TransactionCard/TransactionCard.jsx
+++ src/components/TransactionCard/TransactionCard.jsx
@@ -1,15 +1,21 @@
 import React from 'react'
 import './TransactionCard.css'
 import { Link } from 'react-router-dom';
+import { deleteTransaction } from '../../api/transactions';
 
 function TransactionCard({transaction}) {
   const { id, date, amount } = transaction;
+
+  const deleteButton = (id) => {
+    deleteTransaction(id);
+  }
+
   return (
     <div className='transation-card'>
       <span className="date">{ date }</span>
       <span className='amount'>$ { amount }</span>
       <div className="options">
-        <button className='delete-button'>
+        <button className='delete-button' onClick={ () => deleteButton(id) }>
           <ion-icon name="trash-outline"></ion-icon>
         </button>
         <Link to={`/transactions/edit/${id}`}>
