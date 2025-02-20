8790dbf6ad7e8753373c3a1944d1cf11c810109c
Monica-R <moniroka_339@hotmail.com>
2025-02-20 22:17:44 +0000

    feat: about page completed.

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,7 +2,36 @@ import React from 'react'
 
 function About() {
   return (
-    <div>About</div>
+    <div className='about'>
+      <h1 className='about__h1'>
+         <ion-icon name="information-circle-outline"></ion-icon> About Expenso
+      </h1>
+      <p className='about__p'>
+        Expenso is a personal finance application designed 
+        to help users manage their income and expenses in 
+        a simple and efficient way. It allows users to 
+        record transactions, categorize them and visualize 
+        their impact on the balance sheet. The app features 
+        dynamic graphs that facilitate the analysis of 
+        financial movements and an intuitive interface to 
+        perform operations such as adding, editing and 
+        deleting transactions. Expenso seeks to provide 
+        clarity on the user's financial situation, 
+        helping them to make more informed decisions 
+        about their money.
+      </p>
+      <div className="author">
+        <span>Developer app: <a className='author-link' href="https://github.com/Monica-R">Mónica R. with ❤</a></span>
+        <span>Languages and technologies used:</span>
+        <div className='tech-icons'>
+          <ion-icon name="logo-css3"></ion-icon>
+          <ion-icon name="logo-javascript"></ion-icon>
+          <ion-icon name="logo-nodejs"></ion-icon>
+          <ion-icon name="logo-react"></ion-icon>
+          <ion-icon name="logo-github"></ion-icon>
+        </div>
+      </div>
+    </div>
   )
 }
 
